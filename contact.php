7bdac58628544874a860904019d6f0f7dd4a8670
Grigorean-Silviu <102952429+Grigorean-Silviu@users.noreply.github.com>
2022-04-29 08:10:18 +0300

    29.04.2022

--- contact.php
+++ contact.php
@@ -1,14 +1,11 @@
 <!DOCTYPE html>
 <html lang="ro">
 <head>
-    <title>Contact</title>
-    <link rel="stylesheet" type="text/css" href="assets/css/index.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/nav.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/footer.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/column.css">
+    <title>Inregistrare</title>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="assets/boostraq/css/bootstrap.css"   crossorigin="anonymous">
+    <link rel="icon" type="image/x-icon" href="assets/images/logo.png">
+
+    <link rel="stylesheet" type="text/css" href="assets/css/contact.css">
 
     <meta name="description" content="Contact magazin de instrumente pentru fanfara">
     <meta name="keywords" content="fanfara, instrumente pentru incepatori, teorie, practica, instrumente de suflat,magazin de instrumente, magazin pentru fanfara">
@@ -17,73 +14,54 @@
 </head>
 <body>
 
-<nav class="navb">
-    <div class="container">
-        <ul class="ulnav">
-            <li><img class="imagine" src="assets/images/logo.png" alt="Trumpet"></li>
-            <li class="dropdown">
-                <button class="dropbtn">Contul meu</button>
-                <div class="dropdown-content">
-                    <a href="#">Intra in cont</a>
-                    <a href="teorie.php">Teorie</a>
-                    <a href="practica.php">Practica</a>
-                    <a href="magazin.php">Magazin de instrumente</a>
-                </div>
-            <li><a class="active" href="contact.php">Contact</a></li>
-            <li><a href="despre.php">Despre</a></li>
-            <li><a href="index.php">Acasa</a></li>
-        </ul>
-    </div>
-</nav>
-
-
-
-<div class="header container">
-
-    <h1>Contact</h1>
-    <div class="topleft "><img class="cheiasol" style="width: 48px" src="assets/images/cheiasol.png" alt="cheia sol"></div>
-
+<div class="container">
+    <div class="title">Inregistrare</div>
+    <form action="#">
+        <div class="user-details">
+            <div class="input-box">
+                <span class="details">Numele</span>
+                <input type="text" placeholder="ex. Grigorean Silviu" required>
+            </div>
+            <div class="input-box">
+                <span class="details">Numele de utilizator</span>
+                <input type="text" placeholder="ex. Grigorean Silviu" required>
+            </div>
+            <div class="input-box">
+                <span class="details">Email</span>
+                <input type="text" placeholder="ex. @gmail.com" required>
+            </div>
+            <div class="input-box">
+                <span class="details">Numarul de telefon</span>
+                <input type="text" placeholder="ex. 0774021042" required>
+            </div>
+            <div class="input-box">
+                <span class="details">Parola</span>
+                <input type="text" placeholder="Scrie o parola" required>
+            </div>
+            <div class="input-box">
+                <span class="details">Comfirma Parola</span>
+                <input type="text" placeholder="Rescrie Parola" required>
+            </div>
+        </div>
+        <div class="gender-details">
+            <input type="radio" name="gender" id="dot-1">
+            <input type="radio" name="gender" id="dot-2">
+            <span class="gender-title">Gen</span>
+            <div class="category">
+                <label for="dot-1">
+                    <span class="dot one"></span>
+                    <span class="gender">Masculin</span>
+                </label>
+                <label for="dot-2">
+                    <span class="dot two"></span>
+                    <span class="gender">Feminin</span>
+                </label>
+            </div>
+        </div>
+        <div class="button">
+            <input type="submit" value="Register">
+        </div>
+    </form>
 </div>
-
-
-<address class="container">
-    Mail <a href="mailto:silviu.grigorean01@e-uvt.ro">Grigorean Silviu</a><br>
-    Visit us at:<br>
-    Example.com<br>
-</address>
-
-<footer class="footer-dark">
-    <div class="footer-container">
-        <div class="first width-33">
-            <h4 class="title">Navigare</h4>
-            <ul class="colfirst">
-
-                <li><a href="index.php">Acasa</a></li>
-                <li><a href="despre.php">Despre</a></li>
-                <li><a href="contact.php">Contact</a></li>
-
-            </ul>
-
-        </div>
-
-        <div class="second width-33">
-            <h4 class="title">Invata</h4>
-            <ul class="col">
-
-                <li><a href="teorie.php">Teorie</a></li>
-                <li><a href="practica.php">Practica</a></li>
-
-            </ul>
-        </div>
-
-        <div class="third width-33">
-            <h4 class="title">Company Name</h4></div>
-
-    </div>
-</footer>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-
 </body>
 </html>
